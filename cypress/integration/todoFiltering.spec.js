791489635a9e62647d4b20551015449b1920e052
unknown <fernandes.bcc@gmail.com>
2021-03-22 19:51:28 +0000

    first commit

---
+++ cypress/integration/todoFiltering.spec.js
@@ -0,0 +1,33 @@
+import {TodoPage} from '../page-objects/todoPage'
+
+describe('filtering', function() {
+  const todoPage = new TodoPage()
+
+  beforeEach(() => {
+    todoPage.navigate()
+
+    todoPage.addTodo('Clean room')
+    todoPage.addTodo('Learn JavaScript')
+    todoPage.addTodo('Use Cypress')
+
+    todoPage.toggleTodo(1)
+  })
+
+  it('should filter "Active" correctly', () => {
+    todoPage.showOnlyActiveTodos()
+
+    todoPage.validateNumberOfTodosShown(2)
+  })
+
+  it('should filter "Completed" correctly', () => {
+    todoPage.showOnlyCompletedTodos()
+
+    todoPage.validateNumberOfTodosShown(1)
+  })
+
+  it('should filter "All" correctly', () => {
+    todoPage.showAllTodos()
+
+    todoPage.validateNumberOfTodosShown(3)
+  })
+})
